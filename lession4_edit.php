9c337ee2fc79908156636ffd6ddb14e49cbe5738
dangtinh97 <36624269+dangtinh97@users.noreply.github.com>
2019-06-05 17:45:33 +0700

    Update lession4_edit.php

--- lession4_edit.php
+++ lession4_edit.php
@@ -1,73 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Bài 4 Sắp xếp giá trị trong data</title>
 	<style>
 		table, th, td {
 			padding: 5px;
 			border: 1px solid black;
 			border-collapse: collapse;
 		}
+		input{
+			cursor: pointer;
+			background-color: #5454ff;
+			transition: all .2s ease-in-out; 
+			padding: 5px;
+			border: 1px;
+			color: white;
+			margin: 4px 2px;
+
+		}
+		input:hover{
+			
+			transform: scale(1.1); 
+			background-color: #2b2bff;
+		}
 	</style>
 </head>
 <body>
-	<table>
-		<tr>
-			<th>ID</th>
-			<th>NAME</th>
-			<th>PRICE</th>
-			<th>QUANTITY</th>
-			<th>ORDER</th>
-			<th>SUM</th>
-		</tr>
-		<?php
-		$data=file_get_contents("./data.json");
-		$data=json_decode($data,true);
-		foreach ($data as $key) {
-			echo('<tr><td>'.$key["id"].'</td><td>'.$key["name"].'</td><td>'.$key["price"].'</td><td>'.$key["quantity"].'</td><td>'.$key["order"].'</td><td>'.$key["price"]*$key["order"].'</td></tr>');
-		}
-		?>
-	</table>
-	<form method="post">
-		<input type="submit" name="priceh" value="PRICE >>">
-		<input type="submit" name="pricel" value="PRICE <<">
-		<input type="submit" name="orderh" value="ORDER >>">
-		<input type="submit" name="orderl" value="ORDER <<">
-		<input type="submit" name="totalh" value="TOTAL MONEY >>">
-		<input type="submit" name="totall" value="TOTAL MONEY <<">
+	<div class="main">
+		<div class="table1">
+			<table>
+			<!-- 	<tr>
+					<th>ID</th>
+					<th>NAME</th>
+					<th>PRICE</th>
+					<th>QUANTITY</th>
+					<th>ORDER</th>
+					<th>SUM</th>
+				</tr> -->
+				<?php
+				$data=file_get_contents("./data.json");
+				$data=json_decode($data,true);
+				show($data);
+				?>
+			</table>
+			<form method="post">
+				<input type="submit" name="priceh" value="PRICE ⬆️">
+				<input type="submit" name="pricel" value="PRICE ⬇">
+				<input type="submit" name="orderh" value="ORDER ⬆️">
+				<input type="submit" name="orderl" value="ORDER ⬇">
+				<input type="submit" name="totalh" value="TOTAL MONEY ⬆️">
+				<input type="submit" name="totall" value="TOTAL MONEY ⬇">
 
 
-	</form>
-	<?php
-if(isset($_POST["priceh"])){ // click button tang theo gia
+			</form>
+		</div>
+		<div class="table2">
+			<?php
+if(isset($_POST["priceh"])){
 	show(sortA_Z($data,"price"));
 }
 //============================
-if(isset($_POST["pricel"])){ // click button tang theo gia
+if(isset($_POST["pricel"])){ 
 	show(sortZ_A($data,"price"));
 }
 //============================
-if(isset($_POST["orderh"])){ // click button tang theo gia
+if(isset($_POST["orderh"])){ 
 	show(sortA_Z($data,"order"));
 }
 //============================
-if(isset($_POST["orderl"])){ // click button tang theo gia
+if(isset($_POST["orderl"])){ 
 	show(sortZ_A($data,"order"));
 }
 //============================click 
-if(isset($_POST["totalh"])){ // click button tang theo gia
-	for ($i=0; $i <count($data) ; $i++) { 
-		$data[$i]["sum"]=$data[$i]["price"]*$data[$i]["order"];
-	}
-	show(sortA_Z($data,"sum"));
+if(isset($_POST["totalh"])){ 
+	show(sortSumA_Z($data,"order"));
 }
 //============================click 
-if(isset($_POST["totall"])){ // click button tang theo gia
-	for ($i=0; $i <count($data) ; $i++) { 
-		$data[$i]["sum"]=$data[$i]["price"]*$data[$i]["order"];
-	}
-	show(sortZ_A($data,"sum"));
+if(isset($_POST["totall"])){ 
+	show(sortSumZ_A($data));
 }
 //==============function show
 function show($array_sort){
@@ -81,9 +93,45 @@ function show($array_sort){
 	<th>SUM</th>
 	</tr>';
 	foreach ($array_sort as $key) { 
-		echo('<tr><td>'.$key["id"].'</td><td>'.$key["name"].'</td><td>'.$key["price"].'</td><td>'.$key["quantity"].'</td><td>'.$key["order"].'</td><td>'.$key["price"]*$key["order"].'</td></tr>');
+		echo('<tr><td>'.$key["id"].'</td><td>'.$key["name"].'</td><td>'.$key["price"].'</td><td>'.$key["quantity"].'</td><td>'.$key["order"].'</td><td>'.$key["price"]*$key["quantity"].'</td></tr>');
 	}
 	echo '</table>';
+}
+//=============sap xem tong
+function sortSumA_Z($data){
+	$p_min=0;
+	$count=count($data);
+	for($i = 0; $i < $count-1; $i++) {
+		for($j = $i+1; $j < $count; $j++) {
+			$a=$data[$i]["price"]*$data[$i]["quantity"];
+			$b=$data[$j]["price"]*$data[$j]["quantity"];
+			if($b <$a ) {
+				$p_min = $data[$j];
+				$data[$j] = $data[$i];
+				$data[$i] = $p_min;
+			}
+		}
+
+	}
+	return $data;
+}
+//===========================
+function sortSumZ_A($data){
+	$p_max=0;
+	$count=count($data);
+	for($i = 0; $i < $count-1; $i++) {
+		for($j = $i+1; $j < $count; $j++) {
+			$a=$data[$i]["price"]*$data[$i]["quantity"];
+			$b=$data[$j]["price"]*$data[$j]["quantity"];
+			if($b >$a ) {
+				$p_max = $data[$j];
+				$data[$j] = $data[$i];
+				$data[$i] = $p_max;
+			}
+		}
+
+	}
+	return $data;
 }
 //=============sap xem tang 
 function sortA_Z($data,$field){
@@ -113,10 +161,11 @@ function sortZ_A($data,$field){
 				$data[$i] = $p_max;
 			}
 		}
-
 	}
 	return $data;
 }
 ?>
+</div>
+</div>
 </body>
 </html>
